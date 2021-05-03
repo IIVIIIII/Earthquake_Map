c02fdf31262dd2d97d1236a188fefc4eeadb2d81
IIVIIIII <kelwalls@gmail.com>
2021-05-02 18:15:36 -0700

    remove some comments

--- static/js/logic.js
+++ static/js/logic.js
@@ -69,11 +69,7 @@ d3.json(queryUrl).then(function(data) {
     layers: [darkmap, earthquakes]
   });
   
-//   // Pass our map layers into our layer control
-//   // Add the layer control to the map
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap);
+
   
 
 
@@ -82,66 +78,3 @@ d3.json(queryUrl).then(function(data) {
 
 
 });
-
-// function createFeatures(earthquakeData) {
-
-//     quakes = [];
-
-//     earthquakeData.forEach(q => {
-//         quakes.push(
-//             L.circle([q.geometry.coordinates[0],q.geometry.coordinates[1]], {
-//                 fillOpacity: 0.75,
-//                 color: "white",
-//                 fillColor: "purple",
-//                 radius: 10000
-//             })
-//         )
-//     })
-
-//     var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//         tileSize: 512,
-//         maxZoom: 18,
-//         zoomOffset: -1,
-//         id: "mapbox/streets-v11",
-//         accessToken: API_KEY
-//     });
-
-//     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//         maxZoom: 18,
-//         id: "dark-v10",
-//         accessToken: API_KEY
-//     });
-
-//     var earthquakes = L.layerGroup(quakes);
-
-//     var baseMaps = {
-//         "Street Map": streetmap,
-//         "Dark Map": darkmap
-//     };
-
-//     var overlayMaps = {
-//         Earthquakes: earthquakes
-//     };
-
-//     var myMap = L.map("map", {
-//         center: [
-//           37.09, -95.71
-//         ],
-//         zoom: 5,
-//         layers: [streetmap, earthquakes]
-//     });
-
-//     L.control.layers(baseMaps, overlayMaps, {
-//         collapsed: false
-//     }).addTo(myMap);
-
-//     console.log
-
-//     // layer.bindPopup("<h3>" + feature.properties.place +
-//     //   "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-        
-
-// }
-
