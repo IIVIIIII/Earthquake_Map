c8f84123f6ec59f1ad7a537355b4476134ada560
IIVIIIII <kelwalls@gmail.com>
2021-05-03 14:16:41 -0700

    something like a legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,5 @@
-// Store our API endpoint inside queryUrl
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
 
     console.log(data)
@@ -17,6 +15,13 @@ d3.json(queryUrl).then(function(data) {
         .domain(d3.extent(data.map(q => parseFloat(q.geometry.coordinates[2]))))
         .interpolator(d3.interpolatePlasma)
 
+    var limitScale = d3.scaleLinear()
+        .domain([0, 8])
+        .range(d3.extent(data.map(q => parseFloat(q.geometry.coordinates[2]))))
+        
+
+    console.log(d3.extent(data.map(q => parseFloat(q.geometry.coordinates[2]))))
+
   var quakes = [];
 
   data.forEach(q => {
@@ -29,7 +34,7 @@ d3.json(queryUrl).then(function(data) {
         fillColor: colorScale(q.geometry.coordinates[2]),
         radius: sizeScale(q.properties.mag)
       })
-      .bindPopup(`<h5>Location: ${q.properties.place}</h5><h5>Time: ${Date(q.properties.time)}</h5><h5>Magnitude: ${q.properties.mag}</h5><h5>Depth: ${q.geometry.coordinates[2]}</h5>`)
+      .bindPopup(`<h5>Location: ${q.properties.place}</h5><h5>Magnitude: ${q.properties.mag}</h5><h5>Depth: ${q.geometry.coordinates[2]}</h5>`)
     );
     })
   
@@ -56,6 +61,32 @@ d3.json(queryUrl).then(function(data) {
     zoom: 4,
     layers: [darkmap, earthquakes]
   });
+
+  var legend = L.control({ position: "bottomright" });
+  legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = [limitScale(0), limitScale(1), limitScale(2), limitScale(3), limitScale(4), limitScale(5), limitScale(6), limitScale(7), limitScale(8)];
+      var colors = [colorScale(limits[0]), colorScale(limits[1]), colorScale(limits[2]), colorScale(limits[3]), colorScale(limits[4]), colorScale(limits[5]), colorScale(limits[6]), colorScale(limits[7]), colorScale(limits[8])];
+      var labels = [];
+
+          // Add min & max
+  var legendInfo = "<h1>Median Income</h1>" +
+  "<div class=\"labels\">" +
+    "<div class=\"min\">" + limits[0] + "</div>" +
+    "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+  "</div>";
+
+div.innerHTML = legendInfo;
+
+limits.forEach(function(limit, index) {
+  labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+});
+
+  div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+  return div;
+  };
+  
+  legend.addTo(myMap);
   
 
 });
